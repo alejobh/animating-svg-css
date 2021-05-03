e3c33200b930a4e0a98fe3060504189c2c172f01
Alejandro Bermúdez <alejo0395@gmail.com>
2021-05-03 03:00:19 -0500

    code clean

--- second/index.js
+++ second/index.js
@@ -1,7 +1,7 @@
 const logo = document.querySelectorAll('#code-lines line')
 
 for(let i = 0; i< logo.length; i++) {
-  console.log(`Element ${i+1} is ${logo[i].getTotalLength()} ${logo[i]}`)
+  console.log(`Element ${i+1} is ${logo[i].getTotalLength()}`)
 }
 
 const audio = document.getElementById('audio');
@@ -14,4 +14,4 @@ function play() {
   } else {
     audio.pause();
   }
-};
+}
