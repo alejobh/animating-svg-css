c4d12c59ba07a81fa14ffde68a60925aa832d331
Alejandro Bermúdez <alejo0395@gmail.com>
2021-05-03 02:59:06 -0500

    code clean

--- second/index.js
+++ second/index.js
@@ -1,18 +1,16 @@
 const logo = document.querySelectorAll('#code-lines line')
 
 for(let i = 0; i< logo.length; i++) {
-  console.log(`Element ${i+1} is ${logo[i].getTotalLength()} ${logo[i]}`)
+  console.log(`Element ${i+1} is ${logo[i].getTotalLength()}`)
 }
 
 const audio = document.getElementById('audio');
-audio.currentTime = 0.0;
-audio.volume = 0.3;
+audio.volume = 0.2;
 
-document.getElementById('audio-btn').addEventListener('click', () => {
+function play(){
   if(audio.paused){
     audio.play();
   } else {
     audio.pause();
   }
-});
-
+}
