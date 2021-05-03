b61fa7cede24b235119182b2bc6e3bf89f493ec2
Alejandro Bermúdez <alejo0395@gmail.com>
2021-05-03 02:55:46 -0500

    html 5 audio

--- second/index.js
+++ second/index.js
@@ -4,19 +4,14 @@ for(let i = 0; i< logo.length; i++) {
   console.log(`Element ${i+1} is ${logo[i].getTotalLength()} ${logo[i]}`)
 }
 
-var soundFile = document.createElement("audio");
-soundFile.preload = "auto";
+const audio = document.getElementById('audio');
+audio.currentTime = 0.0;
+audio.volume = 0.3;
 
-var audio = document.createElement("source");
-audio.src = "sound.mp3";
-soundFile.appendChild(audio);
-
-soundFile.load();
-soundFile.volume = 0.0;
-
-document.getElementById('sound').addEventListener('click', () => {
-  soundFile.currentTime = 0.0;
-  soundFile.volume = 0.3;
-  soundFile.play();
-})
-
+function play() {
+  if(audio.paused){
+    audio.play();
+  } else {
+    audio.pause();
+  }
+};
